6d642f1dc833b931899855568fddcc415cf46496
Killian Herlant <killian.herlant@gmail.com>
2023-09-27 19:48:57 +0200

    finished add user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
-
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,11 +14,32 @@ class UserController extends AbstractController
 {
     #[Route('/users', name: 'app_users')]
     public function index(
-        
+        UserRepository $userRepository
     ): Response
     {
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'users' => $userRepository->findAll()
+        ]);
+    }
+
+    #[Route('/users/new', name: 'app_users_new')]
+    public function add(
+        Request $request,
+        EntityManagerInterface $entityManagerInterface
+    ): Response
+    {
+        $form = $this->createForm(UserType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) 
+        {
+            $user = $form->getData();
+            $entityManagerInterface->persist($user);
+            $entityManagerInterface->flush();
+            $this->addFlash("success", "Vous avez bien créé votre compte client.");
+            return $this->redirectToRoute("app_users");
+        }
+        return $this->render('user/new.html.twig', [
+            "form" => $form,
         ]);
     }
 }
